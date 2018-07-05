89bf5f5d35dd3c3b686c82183f8f552bce6e4b8f
Faiz Faidurrahman <faizfaidurrahman@gmail.com>
2018-07-05 16:40:25 +0800

    feat($newClass): A new class to verify apostille
    A new class to verify apostille

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export * from './src/Apostille';
 export * from './src/PublicApostille';
-// export * from './src/Verifier';
+export * from './src/Verifier';
 export * from './src/Initiator';
 export * from './src/hashFunctions';
 export * from './src/Sinks';
