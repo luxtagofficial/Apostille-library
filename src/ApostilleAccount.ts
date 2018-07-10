435b126e64053a570933f52884b537ad80b2599e
Faiz Faidurrahman <faizfaidurrahman@gmail.com>
2018-07-10 18:17:49 +0800

    fix(ApostilleAccounts): getCreationTransaction is working properly
    getCreationTransaction now can differentiate transaction type

--- src/ApostilleAccount.ts
+++ src/ApostilleAccount.ts
@@ -1,15 +1,15 @@
-import { findIndex, sortBy, toUpper } from 'lodash';
-import { AccountHttp, BlockchainHttp, PublicAccount } from 'nem2-sdk';
+import { sortBy } from 'lodash';
+import { AccountHttp, Address, BlockchainHttp, PublicAccount, TransactionType } from 'nem2-sdk';
 
 export class ApostilleAccount {
     /**
-     * @param account
+     * @param {PublicAccount} publicAccount
      */
     constructor(
         /**
          * The account apostille public account.
          */
-        public readonly account: PublicAccount,
+        public readonly publicAccount: PublicAccount,
         ) {
         }
 
@@ -24,7 +24,7 @@ export class ApostilleAccount {
     public getCosignatories(urls: string): Promise<object[]> {
         const accountHttp = new AccountHttp(urls);
         return new Promise(async (resolve, reject) => {
-            accountHttp.getMultisigAccountInfo(this.account.address).subscribe(
+            accountHttp.getMultisigAccountInfo(this.publicAccount.address).subscribe(
                 (accountInfo) => {
                     resolve(accountInfo.cosignatories);
                 },
@@ -43,20 +43,44 @@ export class ApostilleAccount {
     public getCreationTransaction(urls: string): Promise<any> {
         const accountHttp = new AccountHttp(urls);
         return new Promise(async (resolve, reject) => {
-            accountHttp.getAccountInfo(this.account.address).subscribe(
+            accountHttp.getAccountInfo(this.publicAccount.address).subscribe(
                 (accountInfo) => {
                     const blockchainHttp = new BlockchainHttp(urls);
                     const firstTransactionBlock = accountInfo.addressHeight.lower;
+                    // find the first block of this account
                     blockchainHttp.getBlockTransactions(firstTransactionBlock).subscribe(
                         (block) => {
-                            // sort the block by index
-                            const sortedBlock = sortBy(block, ['transactionInfo.index']);
-                            // find the first index of current address from the blockTransactions
-                            const creationTransactionIndex = findIndex(sortedBlock, (o) => {
-                                return o.recipient.address === toUpper(this.account.address.plain());
-                            });
+                            const filteredTransaction: any[] = [];
+                            for (const transaction of block) {
+                                if (transaction.type === TransactionType.TRANSFER) {
+                                    const address = Address.createFromRawAddress(transaction.recipient.address);
+                                    if (this.equals(address)) {
+                                        filteredTransaction.push(transaction);
+                                    }
+                                } else if (transaction.type === TransactionType.AGGREGATE_COMPLETE) {
+                                    for (const innerTransaction of transaction.innerTransactions) {
+                                        if (innerTransaction.type === TransactionType.TRANSFER) {
+                                            const address = Address.createFromRawAddress(
+                                                innerTransaction.recipient.address);
+                                            if (this.equals(address)) {
+                                                filteredTransaction.push(transaction);
+                                            }
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
 
-                            resolve(sortedBlock[creationTransactionIndex]);
+                            // sort the block by index
+                            const sortedTransaction = sortBy(filteredTransaction, ['transactionInfo.index']);
+                            if (sortedTransaction[0].type === TransactionType.AGGREGATE_COMPLETE) {
+                                // if the smallest index is aggregate transaction, then sort innertransaction by index
+                                const sortedAggregateTransaction = sortBy(
+                                    sortedTransaction[0].innerTransactions, ['transactionInfo.index']);
+                                resolve(sortedAggregateTransaction[0]);
+                            }
+
+                            resolve(sortedTransaction[0]);
                         },
                         (err) => {
                             console.error(err.message);
@@ -68,6 +92,15 @@ export class ApostilleAccount {
                     reject(undefined);
                 });
         });
+    }
+
+    /**
+     * Compares address for equality.
+     * @param Address
+     * @returns {boolean}
+     */
+    public equals(address: Address) {
+        return this.publicAccount.address.plain() === address.plain();
     }
 
 }
